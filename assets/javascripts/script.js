7f3cb0ffdab6c60d4d9bf2f26d6d6ea81f5c99a3
Alex Tsun <alextsun@cs.washington.edu>
2015-07-25 12:52:36 -0700

    added fake resume

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,18 +1,25 @@
 
 $(document).ready(function() {
 
-    // load correct html based on user click
+    // initially start w/ home
+    loadContent("home");
+
+
+    // when click on tab, load content
     $("#cssmenu li").click(function() {
-        var id = this.id;
-        if (id !== "") {
-            var file = id + ".html";
-            $.ajax({
-                url: file,
-                type: "GET",
-                success: function(result) {
-                    $("#content").html(result);
-                }
-            });
-        }
+       loadContent(this.id);
     });
 });
+
+function loadContent(id) {
+    if (id !== "") {
+        var file = id + ".html";
+        $.ajax({
+            url: file,
+            type: "GET",
+            success: function(result) {
+                $("#content").html(result);
+            }
+        });
+    }
+}
